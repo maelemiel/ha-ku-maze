b48f82a5b682d6321ccdcff05bfe5460c2bf6961
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-05-21 11:57:50 +0200

    [ADD]: algoritm for switching walls colors

--- haku/Assets/RotateImage.cs
+++ haku/Assets/RotateImage.cs
@@ -10,18 +10,29 @@ public class RotateImage : MonoBehaviour
     private float targetAngle = 90f;
 
     private bool isRotating = false;
+    private bool isWhite = true;
     private Quaternion startRotation;
     private Quaternion targetRotation;
 
+    private GameObject[] blackWalls;
+    private GameObject[] whiteWalls;
+
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
         startRotation = transform.rotation;
+        blackWalls = GameObject.FindGameObjectsWithTag("BlackWall");
+        whiteWalls = GameObject.FindGameObjectsWithTag("WhiteWall");
+        SwitchWalls();
     }
 
     // Update is called once per frame
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.E) && !isRotating) {
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && !isRotating)
+        {
             StartCoroutine(RotateToDestination());
+            SwitchWalls();
         }
     }
 
@@ -32,7 +43,8 @@ public class RotateImage : MonoBehaviour
         targetRotation = startRotation * Quaternion.Euler(0f, 0f, targetAngle);
 
         float t = 0f;
-        while (t < 1f) {
+        while (t < 1f)
+        {
             t += Time.deltaTime * rotationSpeed;
             transform.rotation = Quaternion.Lerp(startRotation, targetRotation, t);
             yield return null;
@@ -43,4 +55,38 @@ public class RotateImage : MonoBehaviour
 
         isRotating = false;
     }
+
+    private void SwitchWalls()
+{
+    if (isWhite)
+    {
+        foreach (GameObject blackWall in blackWalls)
+        {
+            blackWall.SetActive(true);
+            SpriteRenderer wallRenderer = blackWall.GetComponent<SpriteRenderer>();
+            if (wallRenderer != null)
+            {
+                wallRenderer.color = Color.white;
+            }
+        }
+        foreach (GameObject whiteWall in whiteWalls)
+        {
+            whiteWall.SetActive(false);
+        }
+        isWhite = false;
+    }
+    else
+    {
+        foreach (GameObject blackWall in blackWalls)
+        {
+            blackWall.SetActive(false);
+        }
+        foreach (GameObject whiteWall in whiteWalls)
+        {
+            whiteWall.SetActive(true);
+        }
+        isWhite = true;
+    }
+}
+
 }
